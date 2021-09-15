51ab9fb6e980dc1436e137c6b24dd3f54de5da7a
Zheng Wei <wzweizheng@qq.com>
2021-09-15 20:35:50 +0800

    fix bug

--- script.sh
+++ script.sh
@@ -3,16 +3,16 @@ mkdir ../result
 cd demo1
 bash script.sh
 mkdir ../../result/demo1
-mv layer* ../../result/demo1
 cd ../
 cd demo2
 bash script.sh
 mkdir ../../result/demo2
-mv layer* ../../result/demo2
 cd ../
-cd 'demo2_2'
+cd demo2_2
 bash script.sh
 mkdir ../../result/demo2_2
-mv layer* ../../result/demo2_2
 cd ../
+mv demo1/layer* ../../result/demo1
+mv demo2/layer* ../../result/demo1
+mv demo2_2layer* ../../result/demo1
 date
