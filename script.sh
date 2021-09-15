97a19ee592de94f8b9567c32e01b7705632c85ee
Zheng Wei <wzweizheng@qq.com>
2021-09-15 21:10:45 +0800

    fix bug

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ cd demo2_2
 bash script.sh
 mkdir ../../result/demo2_2
 cd ../
-mv demo1/layer* ../../result/demo1
-mv demo2/layer* ../../result/demo1
-mv demo2_2layer* ../../result/demo1
+mv demo1/layer* ../result/demo1/
+mv demo2/layer* ../result/demo2/
+mv demo2_2/layer* ../result/demo2_2/
 date
